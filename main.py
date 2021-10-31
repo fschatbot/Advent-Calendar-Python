3fd534063e0770e2866ce89890f37232b43462e7
fschatbot <s.himanshu.sultania@fountainheadschools.org>
2021-10-31 14:46:36 +0530

    Changes to how modules are treated and executed

--- main.py
+++ main.py
@@ -1,41 +1,81 @@
 import requests
 import importlib
-
-from requests import models
 import config
-from os.path import exists
+from os import path, makedirs
 from time import time
 
-if not exists(f'{config.year}/{config.day}.txt') or config.force_find:
-	response = requests.get(f'https://adventofcode.com/{config.year}/day/{config.day}/input', cookies={'session':config.session_cookie})
-	raw_data = response.text.strip()
-	# Save the data for backup
-	if response.status_code == 200:
-		with open(f'{config.year}/{config.day}.txt', 'w') as file:
-			file.write(raw_data)
-else:
-	with open(f'{config.year}/{config.day}.txt', 'r') as file:
-		raw_data = ''.join(file.readlines())
+folder_path = f'{config.year}/{config.day}/'
+txt_path = f'{config.year}/{config.day}/input.txt'
+module_path = f'{config.year}/{config.day}/runner'
+raw_data = None
 
+# collect the data
+def collect_data(force) -> None:
+	"""A simple function to calculate data"""
+	global raw_data
+	if not path.exists(txt_path) or force:
+		response = requests.get(f'https://adventofcode.com/{config.year}/day/{config.day}/input', cookies={'session':config.session_cookie})
+		raw_data = response.text.strip()
+		# Save the data for backup
+		if response.status_code == 200:
+			with open(txt_path, 'w') as file:
+				file.write(raw_data)
+		elif response.status_code == 400 and raw_data == 'Puzzle inputs differ by user.  Please log in to get your puzzle input.':
+			print("Hey looks like you need to provide a session cookie!")
+			exit()
+		else:
+			print(response.status_code, raw_data)
+	else:
+		with open(txt_path, 'r') as file:
+			raw_data = ''.join(file.readlines())
+
+# Create a setup
+def setup() -> None:
+	"""This function creates the folder with the template and the input sheet already in it"""
+	global raw_data
+	try:
+		makedirs(folder_path)
+	except FileExistsError:
+		pass
+	collect_data(True)
+	with open(module_path+'.py','w') as file:
+		with open('runner_template.py','r') as template:
+			lines = template.readlines()
+			file.write(''.join(lines))
 
 # Run the Code
-module = importlib.import_module(f'{config.year}.{config.day}')
-module.raw_data = raw_data
-if not module.completed:
-	print("WARNING: This answer is still not complete and it may be wrong")
+def run_code() -> None:
+	"""This function is responsible for running the code from the given year and day
+	After the complete run of each part the answer is printed in the console with the amount of time it took to run"""
+	module = importlib.import_module(module_path.replace('/','.'))
+	module.raw_data = raw_data
+	if not module.completed:
+		print("\nWARNING: This answer is still not complete and it may be wrong\n")
 
-# Part 1
-start = time()
-answer1 = module.part1(raw_data.split('\n') if module.split_data else raw_data)
-end = time()
-print("The answer to the 1st part is:", answer1)
-# If the total time is above 0.01 secounds then we show it in secounds otherwise we show it in milliseconds
-time_taken = f'{end - start:,.2}s' if end - start > 0.01 else f'{(end - start) * 1000:,.3}ms'
-print(f"The 1st answer was calculated in just {time_taken}")
-# Part 2
-start = time()
-answer2 = module.part2(raw_data.split('\n') if module.split_data else raw_data)
-end = time()
-print("The answer to the 2nd part is:", answer2)
-time_taken = f'{end - start:,.2}s' if end - start > 0.01 else f'{(end - start) * 1000:,.3}ms'
-print(f"The 1st answer was calculated in just {time_taken}")
+	# Part 1
+	start = time()
+	answer1 = module.part1(raw_data.split('\n') if module.split_data else raw_data)
+	end = time()
+	print("The answer to the 1st part is:", answer1)
+	# If the total time is above 0.01 secounds then we show it in secounds otherwise we show it in milliseconds
+	time_taken = f'{end - start:,.2}s' if end - start > 0.01 else f'{(end - start) * 1000:,.3}ms'
+	print(f"The 1st answer was calculated in just {time_taken}")
+	# Part 2
+	start = time()
+	answer2 = module.part2(raw_data.split('\n') if module.split_data else raw_data)
+	end = time()
+	print("The answer to the 2nd part is:", answer2)
+	time_taken = f'{end - start:,.2}s' if end - start > 0.01 else f'{(end - start) * 1000:,.3}ms'
+	print(f"The 1st answer was calculated in just {time_taken}")
+
+if __name__ == "__main__":
+	if not path.exists(folder_path):
+		print("Looks like this day has not been answered yet!")
+		print("I will create a folder with the template for you to answer it ;)")
+		setup()
+		print("I have completed setting it up for you! Enjoy =)")
+	else:
+		print("Collecting Data",end="\r")
+		collect_data(False)
+		print("Running Code...",end="\r")
+		run_code()
