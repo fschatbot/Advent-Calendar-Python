d316bff231db2b1e6438b4301b0a3027f801bb94
fschatbot <s.himanshu.sultania@fountainheadschools.org>
2021-10-31 09:34:01 +0530

    Changed how running the code works

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import importlib
 import config
 from os.path import exists
-
+from time import time
 
 if not exists(f'{config.year}/{config.day}.txt') or config.force_find:
 	response = requests.get(f'https://adventofcode.com/{config.year}/day/{config.day}/input', cookies={'session':config.session_cookie})
@@ -17,5 +17,13 @@ else:
 
 
 # Run the Code
-key = importlib.import_module(f'{config.year}.{config.day}')
-key.main(raw_data)
+module = importlib.import_module(f'{config.year}.{config.day}')
+module.raw_data = raw_data
+start = time()
+answer1 = module.part1(raw_data.split('\n') if module.split_data else raw_data)
+print("The answer to the 1st part is:", answer1)
+print(f"The 1st answer was calculated in just {(time() - start) * 1000:,.3}ms")
+start = time()
+answer2 = module.part2(raw_data.split('\n') if module.split_data else raw_data)
+print("The answer to the 2nd part is:", answer2)
+print(f"The 2nd answer was calculated in just {(time() - start) * 1000:,.3}ms")
