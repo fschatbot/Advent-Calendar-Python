07fa0e2731661fdb058698789b3fe49bdad9cb96
fschatbot <s.himanshu.sultania@fountainheadschools.org>
2021-11-02 09:34:35 +0530

    New Clean Template

--- runner_template.py
+++ runner_template.py
@@ -3,9 +3,7 @@ completed = False
 raw_data = None # Not To be touched
 
 def part1(data):
-	"""The Code is supposed to run here"""
 	pass
 
 def part2(data):
-	"""The Code is supposed to run here"""
 	pass
