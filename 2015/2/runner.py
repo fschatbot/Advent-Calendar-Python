ef46f94687f65f10967c0e81e0c98270ffcd09a8
fschatbot <s.himanshu.sultania@fountainheadschools.org>
2021-11-03 11:31:41 +0530

    answer for day 3 year 2015

---
+++ 2015/2/runner.py
@@ -0,0 +1,21 @@
+split_data = True
+completed = False
+raw_data = None # Not To be touched
+
+def part1(data):
+	paper_needed = 0
+	for line in data:
+		l, w, h = [int(x) for x in line.split('x')]
+		SAarea = 2*l*w + 2*w*h + 2*h*l
+		min_side = min(l*w, w*h, h*l)
+		paper_needed += SAarea + min_side
+	return paper_needed
+
+def part2(data):
+	ribon_needed = 0
+	for line in data:
+		l, w, h = [int(x) for x in line.split('x')]
+		bow = l*w*h
+		wrapping = min(2*l + 2*w, 2*h + 2*w, 2*h + 2*l)
+		ribon_needed += bow + wrapping
+	return ribon_needed
