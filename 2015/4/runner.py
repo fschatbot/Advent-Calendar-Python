231eaff530a2c1e34c5e0f3914526a9b427e9bed
fschatbot <s.himanshu.sultania@fountainheadschools.org>
2021-11-03 12:22:50 +0530

    Answer for day 4 year 2015

---
+++ 2015/4/runner.py
@@ -0,0 +1,17 @@
+import hashlib
+
+split_data = False
+completed = False
+raw_data = None # Not To be touched
+
+def part1(key):
+	# Go through 10 million hashes to find our baby
+	# These will take time as we are brute forcing our way through the hash
+	for i in range(10_000_000):
+		if hashlib.md5((key + str(i)).encode()).hexdigest()[:5] == '00000':
+			return i
+
+def part2(key):
+	for i in range(10_000_000):
+		if hashlib.md5((key + str(i)).encode()).hexdigest()[:6] == '000000':
+			return i
