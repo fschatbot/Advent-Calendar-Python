eef173ac2bb5794c269936143944e27e841de5fd
fschatbot <s.himanshu.sultania@fountainheadschools.org>
2021-11-03 20:14:33 +0530

    Answer for part 1 only day 2 year 2019

---
+++ 2019/2/runner.py
@@ -0,0 +1,45 @@
+split_data = ','
+completed = False
+raw_data = None # Not To be touched
+
+def part1(data):
+	# First we do the replacement
+	data = list(map(int, data))
+	data[1] = 12
+	data[2] = 2
+	# Then we run the program
+	i = 0
+	while i < len(data):
+		if data[i] == 1:
+			data[data[i+3]] = data[data[i+1]] + data[data[i+2]]
+			i += 4
+		elif data[i] == 2:
+			data[data[i+3]] = data[data[i+1]] * data[data[i+2]]
+			i += 4
+		elif data[i] == 99:
+			break
+		else:
+			raise Exception('Invalid opcode')
+	return data[0]
+
+def part2(data):
+	# Almost same from the previous part, but we need to find the right values
+	data = list(map(int, data))
+	data[1] = 12
+	data[2] = 2
+	# Then we run the program
+	i = 0
+	while i < len(data):
+		if data[i] == 1:
+			data[data[i+3]] = data[data[i+1]] + data[data[i+2]]
+			i += 4
+		elif data[i] == 2:
+			data[data[i+3]] = data[data[i+1]] * data[data[i+2]]
+			i += 4
+		elif data[i] == 99:
+			break
+		else:
+			raise Exception('Invalid opcode')
+		if data[0] == 19690720 or data[1] == 19690720:
+			return 100 * data[1] + data[2]
+	return 100 * data[1] + data[2]
