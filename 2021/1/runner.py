672b2b5090b38a5ef873aa84bf42d138e6bf7c50
fschatbot <s.himanshu.sultania@fountainheadschools.org>
2021-12-08 20:05:32 +0530

    Answer To: Part 2, Day 1, Year 2021

--- 2021/1/runner.py
+++ 2021/1/runner.py
@@ -1,5 +1,5 @@
 split_data = True
-completed = False
+completed = True
 raw_data = None # Not To be touched
 
 def part1(data):
@@ -10,4 +10,15 @@ def part1(data):
 	return counter
 
 def part2(data):
-	pass
+	data = list(map(int,data))
+	# First we create the groups we want
+	new_data = []
+	for index, _ in enumerate(data[:-2]):
+		new_data.append(sum(data[index:index+3]))
+	# Now that we have thhe sum of each windows we can now count the increatment
+	counter = 0
+	for index, i in enumerate(new_data[1:]):
+		if new_data[index] < i:
+			counter += 1
+	return counter
+
