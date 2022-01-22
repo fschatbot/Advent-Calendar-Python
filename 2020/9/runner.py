1ab04c2676655dae69d019f52dd85815daa6d07b
fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-01-22 18:38:43 +0530

    Answer for part 2 Detailed Comments have also been added

--- 2020/9/runner.py
+++ 2020/9/runner.py
@@ -1,5 +1,5 @@
 split_data = True
-completed = False
+completed = True
 raw_data = None # Not To be touched
 
 def part1(data):
@@ -22,4 +22,25 @@ def part1(data):
 			return num
 
 def part2(data):
-	pass
+	# We get our target number
+	target = part1(data)
+	# Convert the string to numbers
+	data = list(map(int, data))
+	# The time for required for the solution is O(n^2) for 0.3s
+	# First we loop through all the possible list combinations
+	for i,_ in enumerate(data):
+		# The i+1 makes the list loop get smaller and faster
+		for j in range(i+1, len(data)):
+			# Get the sum of the range
+			setsum = sum(data[i:j])
+			# This saves us time from the list being denied from 2 checks
+			if setsum < target:
+				continue
+			# This saves us a lot of time as it cancels out i as the possible number
+			elif setsum > target:
+				break
+			# We got out set
+			elif setsum == target:
+				# We could use sorting but why waste time
+				# use min to get the smallest number and max to get the largest number
+				return min(data[i:j]) + max(data[i:j])
