97664aa885887d6d29d5e7af02903bc713acd0fc
fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-01-24 09:09:00 +0530

    A faster solution. Not using regex

--- 2015/19/runner.py
+++ 2015/19/runner.py
@@ -12,12 +12,22 @@ def complile_data(data):
 def replace_index(string, index, old_str, new_str, count):
 	return string[:index] + string[index:].replace(old_str, new_str, count)
 
+def find_all(string, sub):
+	# Answer taken from https://stackoverflow.com/a/4664889/13703806
+	if string.find(sub) == -1: return []
+	start = 0
+	while True:
+		start = string.find(sub, start)
+		if start == -1: return
+		yield start
+		start += len(sub)
+
 def replace_molecules(molecule, data):
 	combinations = []
 	for name, replacement in data:
 		# Get all the indexes of the replacement in the molecule
 		# Answer taken from https://stackoverflow.com/a/4664889/13703806
-		indexes = [m.start() for m in re.finditer(name, molecule)]
+		indexes = [*find_all(molecule, name)]
 		for index in indexes:
 			new_molecule = replace_index(molecule, index, name, replacement, 1)
 			combinations.append(new_molecule)
@@ -29,4 +39,19 @@ def part1(data):
 	return len(set(combinations))
 
 def part2(data):
-	pass
+	molecule, replacements  = complile_data(data)
+	NotFound = True
+	lookfors = ['e']
+	steps = 0
+	while NotFound:
+		steps += 1
+		for mole in lookfors:
+			combinations = replace_molecules(mole, replacements)
+			for combination in combinations:
+				if len(combination) > len(molecule):
+					continue
+				if combination == molecule:
+					NotFound = False
+				else:
+					lookfors.append(combination)
+	return steps
