2f0497db2541c55534ed2378370d7a2a895eefa8
fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-01-29 15:25:48 +0530

    Answer to part 2. However it comes with an error

--- 2017/7/runner.py
+++ 2017/7/runner.py
@@ -1,5 +1,6 @@
+from ctypes import Union
 import json
-
+from typing import List
 
 split_data = True
 completed = False
@@ -59,14 +60,69 @@ def process_data(data):
 	tree = cache.copy()
 	cache.clear()
 	return new_data, tree
-	# Now that we have proccessed the data into something more readable lets make it into a tree
-	# We are sorting the list to make sure that the ones without children come on the top
-	# This will allow us the children to be added to the tree
 
 def part1(data):
 	_, tree = process_data(data)
 	return list(tree.keys())[0]
 
+def FindTheOddOneOut(arr):
+	# Count how many times a number appears in the array
+	counter = {}
+	for i in arr:
+		if sum(i) in counter:
+			counter[sum(i)] += 1
+		else:
+			counter[sum(i)] = 1
+	# Find the number that appears only once
+	for num,value in counter.items():
+		if value == 1:
+			# This returns the number that appears only once and the one other number as sample
+			return num, [name for name in counter.keys() if name != num][0]
+	else:
+		return None
+
+def collapes_tower(tower_value):
+	"""
+	This function takes in the tower dict and returns 2 values
+
+	1. (The weight of the tower, The total weight of the children) or int
+
+	> Situation 1: The values are not added up but returned as a tuple. Ex: (int, int)
+	> Situation 2: The value returned is the answer
+	
+	2. Boolean
+
+	> If this is true then it means that the data returned above is the answer and should we handded down the tree instantly
+	"""
+	# If there are no children then we just return the weight
+	if not tower_value['children']: return (tower_value['weight'], 0), False
+
+	# This for loop gets the weight of each child in a list
+	children = tower_value['children'].copy()
+	tower_value['children'] = []
+	for value in children.values():
+		weight, true_return = collapes_tower(value)
+		# A true return means it is the answer to our problem
+		# Hence we skip everything else and return it down the tree and back to the part1 as fast as possible
+		# However if we don't get a true return we simply replace the child with it's weight
+		if true_return: return weight
+		else: tower_value['children'].append(weight)
+	# Next we compare to see if the children have the same weight, If not then boom we found the problem
+	odd_one = FindTheOddOneOut(tower_value['children'])
+	if odd_one:
+		# Now we need to find which child is the odd one out
+		unbalanced_child = [child for child in tower_value['children'] if sum(child) == odd_one[0]][0]
+		# This returns the differnce in weight between the odd one and the other child
+		diff = odd_one[1] - sum(unbalanced_child)
+		# We combine the difference and we get our answer which we pass down the tree
+		print(unbalanced_child[0] + diff)
+		return unbalanced_child[0] + diff, True
+	else:
+		# Return the weight of the children plus the inital weight of the tower and false
+		return (tower_value['weight'], sum(sum(child) for child in tower_value['children'])), False
+
 def part2(data):
 	_, tree = process_data(data)
-	print(json.dumps(tree, indent=4))
+	# We have to loop through each tower and essitally collapsing the children to get the total weight of the towers
+	tower_weight, _ = collapes_tower(tree[list(tree.keys())[0]])
+	return tower_weight
