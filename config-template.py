16136817d41ac20733323b2428cb1e3b8d2d200c
Fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-02-28 21:16:51 +0530

    Fixed a cookie string format

--- config-template.py
+++ config-template.py
@@ -9,7 +9,7 @@ day = 6
 # 3) Next press the reload button(F5) or manually reload that page
 # 4) If done correctly you will see 2 bars one saying 'input' and the other 'favicon.ico'
 # 5) Click on the 'input' bar and then scroll down to the 'Request Headers' section
-# 6) In there look for 'cookie' and you will find a string like so: _ga=xxxx9; _gid=xxxx; session=xxxx
+# 6) In there look for 'cookie' and you will find a string like so: session=xxxx
 # 7) Copy the text after 'session=' and replace it with the xxxx down below
 session_cookie = 'xxxx'
 # If force find is enabled it will call the server for the answer
