373ed5e469e47571c3b5c8be94dd4c81c53cfef2
Fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-03-01 20:56:52 +0530

    A try was made

--- 2015/20.py
+++ 2015/20.py
@@ -12,15 +12,18 @@ def part1(data):
 	# We wan't to find which house gets the gifts as suggested in the puzzle
 	# we will not multiple by 10 to save time, by that I mean very so slithly cause it will take a huge amount of time
 	# If you want a faster solution check https://github.com/Peter200lx/advent-of-code/blob/master/2015/day20.py but beaware I didn't understand hence I didn't use it
-	num = int(data) / 10
-	for i in range( 10_000_000):
-		summed = factor_sum(i)
-		if summed == num:
-			return i
-		elif summed > num:
-			print("The number is too high", i)
-		else:
-			print(i, summed,end='\r')
+	num = int(data) // 10
+	# We are just going to keep on delivering presents to each house until we get to the house we are looking for
+	houses = [0] * num
+	# Loop Through Each Number
+	for i in range(1, num):
+		# Loop through all the houses that this number is going to visit and deliver the present
+		for j in range(i, num, i):
+			houses[j] += i
+	# Check for which house has the number we are looking for
+	for index, house in enumerate(houses):
+		if house == num:
+			return index + 1
 
 def part2(data):
 	pass
