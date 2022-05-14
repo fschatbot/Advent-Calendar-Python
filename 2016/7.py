ba2fadfb2ae944fa70af505e892d59c8a4643b7d
Fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-05-14 12:29:04 +0530

    Finally completed day 7 of year 2016

--- 2016/7.py
+++ 2016/7.py
@@ -1,8 +1,12 @@
+import regex # we will use third party regex library in order to get overlapping regex matches
 import re
 
 split_data = True
-completed = False
+completed = True
 raw_data = None # Not To be touched
+
+# I really needed help from https://github.com/natemago/adventofcode2016/ in order to complete this day
+# This day was somewhat not clear and regex sometimes wanted to work and sometimes not...
 
 def part1(data):
 	supported = 0
@@ -26,5 +30,22 @@ def part1(data):
 	return supported
 
 
+
+def isLineSupported(line):
+	# Split the data to get all the supernets aka (the part not in brackets)
+	# re.split has not be used because it would capture the content inside the brackets aswell
+	supernets = re.sub(r'\[(\w+)\]','|',line).split('|')
+	# Search for aba patterns in the supernets
+	for supernet in supernets:
+		match = regex.findall(r'(\w)(?!\1)(\w)\1', supernet, overlapped=True) # find all overlapping aba patterns
+		if not match: continue # Check if any matches were found
+		babs = [aba[1] + aba[0] + aba[1] for aba in match] # If yes, convert the matches into bab patterns
+		for bab in babs:
+			# Loop through all the bab patterns and check if any are there in any hypernets
+			for hypernet in re.finditer(r'\[(\w+)\]', line):
+				if bab in hypernet.group(1): # If yes, return True because the IP is valid
+					return True
+
 def part2(data):
-	pass
+	# A simple counter for all the times the IP is valid
+	return sum(1 for line in data if isLineSupported(line))
