64e92b7233f0f9f8371163e8110ced427176b77c
Fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-11-27 21:02:23 +0530

    Improved and completed day 3 of year 2019

--- 2019/3.py
+++ 2019/3.py
@@ -1,37 +1,47 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from .intersection import intersection
+from typing import List, Tuple
 
 split_data = True
-completed = 1
+completed = True
 raw_data = None # Not To be touched
+
+def generateMapping(path) -> List[Tuple[int]]:
+	"""Generates a list of coordinate points that the wire takes us to"""
+	coord = [0, 0]
+	points = []
+
+	for ins in path.split(','):
+		val = int(ins[1:])
+		for _ in range(val):
+			if ins.startswith('R'):
+				coord[0] += 1
+			elif ins.startswith('L'):
+				coord[0] -= 1
+			elif ins.startswith('U'):
+				coord[1] += 1
+			elif ins.startswith('D'):
+				coord[1] -= 1
+			points.append(tuple(coord))
+	return points
 
 def part1(data):
 	# Converting line data into points
-	lines = []
-	for line in data:
-		coord = [0, 0]
-		points = [coord.copy()]
-
-		for ins in line.split(','):
-			if ins.startswith('R'):
-				coord[0] += int(ins[1:])
-			elif ins.startswith('L'):
-				coord[0] -= int(ins[1:])
-			elif ins.startswith('U'):
-				coord[1] += int(ins[1:])
-			elif ins.startswith('D'):
-				coord[1] -= int(ins[1:])
-			points.append(coord.copy())
-		
-		lines.extend(zip(*points))
+	wires = [generateMapping(wire) for wire in data]
 	
-	inter_x, inter_y = intersection(*lines) # Finding all the intersections
+	inter = set(wires[0]).intersection(wires[1]) # <-- All the points which are appearing in both the wires
+	# Now we return the smallest manhattam distance
+	return min(abs(x) + abs(y) for x,y in inter)
 
-	# Joining the 2 numpy arrays into a point list
-	points = [*zip(inter_x, inter_y)]
-	return min([abs(x) + abs(y) for x,y in points])
 
 def part2(data):
-	...
+	wires = [generateMapping(wire) for wire in data]
+	intersections = set(wires[0]).intersection(wires[1]) # <-- All the intersection points
+
+	# Convert intersection points to steps in each wire
+	wire1 = [wires[0].index(point)+1 for point in intersections]
+	wire2 = [wires[1].index(point)+1 for point in intersections]
+	
+	# Return the minimum of the total steps taken by each wire to reach an intersection
+	return min(wire1[i] + wire2[i] for i in range(len(intersections)))
