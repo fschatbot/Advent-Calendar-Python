a18db23fa53cefcedd75e5fb1feeea8ea85f4d5a
Fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-12-04 20:41:46 +0530

    Created a new computer but based on the class

--- 2019/intcode.py
+++ 2019/intcode.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List, Tuple, Literal
 
 class InvalidOpCode(Exception):
 	def __init__(self, instructions, ErrIndex):
@@ -119,3 +119,97 @@ def computer3(instructions:list, inputs:List[int]) -> List[int]:
 			raise InvalidOpCode(ins, i)
 	return outputs
 
+class computer4:
+	"""This computer gives more control over the code"""
+	def __init__(self, instructions:List[int]) -> None:
+		self.ins = instructions
+		self.inps = []
+		self.outs = []
+		self.halted = False
+		self.pointer = 0
+	
+	# These functions allow for control
+	def input(self, inp:int) -> None:
+		self.inps.append(inp)
+	
+	# These functions allow the computer to run
+	def getVal(self, index:int, mode:Literal[0, 1]) -> int:
+		"""Return value from index based on the mode provided. 0 stands for positional mode and 1 stands fo literal mode."""
+		return self[self[index]] if mode == '0' else self[index]
+	
+	def __getitem__(self, __index):
+		"""Get instruction from index"""
+		return self.ins[__index]
+	def __setitem__(self, __index, __value):
+		"""Set instruction from index"""
+		self.ins[__index] == __value
+
+	@classmethod
+	def from_instructions(cls, instructions:List[int]) -> """computer4""":
+		return cls([int(x) for x in instructions.copy()])
+
+	# Runner Functions
+	def run_once(self):
+		"""Runs the IntCode once"""
+		if self.halted: raise RuntimeError("The computer has already halted")
+		
+		# Over here we extract the op code and the parameter mode
+		instruction = str(self.ins[i]).zfill(4)
+		op_code = int(instruction[-2:])
+		mode = instruction[:-2][::-1]
+		# Over here we perform the op code instructions
+		if op_code == 1:
+			self[self[i+3]] = self.getVal(i+1, mode[0]) + self.getVal(i+2, mode[1])
+			i += 4
+		elif op_code == 2:
+			self[self[i+3]] = self.getVal(i+1, mode[0]) * self.getVal(i+2, mode[1])
+			i += 4
+		elif op_code == 3:
+			self[self[i+1]] = self.inps.pop(0)
+			i += 2
+		elif op_code == 4:
+			self.outs.append(self.getVal(i+1, mode[0]))
+			i += 2
+		elif op_code == 5:
+			if self.getVal(i+1, mode[0]) != 0:
+				i = self.getVal(i+2, mode[1])
+			else:
+				i += 3
+		elif op_code == 6:
+			if self.getVal(i+1, mode[0]) == 0:
+				i = self.getVal(i+2, mode[1])
+			else:
+				i += 3
+		elif op_code == 7:
+			if self.getVal(i+1, mode[0]) < self.getVal(i+2, mode[1]):
+				self[self[i+3]] = 1
+			else:
+				self[self[i+3]] = 0
+			i += 4
+		elif op_code == 8:
+			if self.getVal(i+1, mode[0]) == self.getVal(i+2, mode[1]):
+				self[self[i+3]] = 1
+			else:
+				self[self[i+3]] = 0
+			i += 4
+		elif op_code == 99:
+			self.halted = True
+		else:
+			raise InvalidOpCode(self, i)
+	
+	def run_till_output(self) -> int:
+		"""Keeps running the code till their is an output"""
+		old_out_len = len(self.outs)
+		while not self.halted and old_out_len == len(self.outs):
+			self.run_once()
+		return self.recent_output
+	
+	def run_till_end(self):
+		"""Keeps running the code till it halts"""
+		while not self.halted:
+			self.run_once()
+	
+	# Properties
+	@property
+	def recent_output(self) -> int:
+		return self.outs[-1]
