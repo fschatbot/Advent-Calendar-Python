0499e65c6b292780bb7fea189e07940d9d6dfe04
Fschatbot <s.himanshu.sultania@fountainheadschools.org>
2023-02-05 19:44:45 +0530

    Improved the code to be much 10x faster and marked it complete

--- 2019/13.py
+++ 2019/13.py
@@ -5,7 +5,7 @@ from .intcode import computer5
 # import os
 
 split_data = ','
-completed = 1
+completed = True
 raw_data = None # Not To be touched
 
 def part1(data):
@@ -40,45 +40,39 @@ def part1(data):
 # 	os.system('cls' if os.name == 'nt' else 'clear')
 # 	print(print_output)
 
-
-def proccessOutput(output):
-	xPos = output[::3]
-	yPos = output[1::3]
-	tile = output[2::3]
-
-	tiles = {}
-	score = 0
-	ball = None, None
-	paddle = None, None
-	for x, y, tile in zip(xPos, yPos, tile):
-		if x == -1 and y == 0:
-			score = tile
-			continue
-		if tile == 4:
-			ball = x, y
-		if tile == 3:
-			paddle = x, y
-		tiles[(x, y)] = tile
-	
-	gameOver = all(tile != 2 for tile in tiles.values())
-	# displayMap(tiles)
-
-	return score, ball, paddle, gameOver
-
-
 def part2(data):
 	data[0] = 2
 	computer = computer5.from_instructions(data)
-	score, ball, paddle, gameOver = 0, (None, None), (None, None), False
-	while not gameOver:
-		computer.run_till_input()
-		score, ball, paddle, gameOver = proccessOutput(computer.outs)
 
+	tiles = {}
+	score = 0
+	ball = (None, None)
+	paddle = (None, None)
+
+	while any(tile == 2 for tile in tiles.values()) or tiles == {}:
+		computer.run_till_input()
+		# Process the output from the computer
+		xPos = computer.outs[::3]
+		yPos = computer.outs[1::3]
+		tile = computer.outs[2::3]
+		for x, y, tile in zip(xPos, yPos, tile):
+			if x == -1 and y == 0:
+				score = tile
+			elif tile == 4:
+				ball = x, y
+			elif tile == 3:
+				paddle = x, y
+			
+			tiles[(x, y)] = tile
+		computer.outs = []
+
+		# Move the paddle
 		if ball[0] < paddle[0]:
 			computer.input(-1)
 		elif ball[0] > paddle[0]:
 			computer.input(1)
 		else:
 			computer.input(0)
-	
+		
+		# displayMap(tiles)
 	return score
